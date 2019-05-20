5b7004bd4978abf6de6f6a464f36c2b2010ffa4a
joaquinto <roejoeodj12@gmail.com>
2019-05-20 14:53:14 +0100

    feature(question endpoint): add question endpoint   [Finishes #166118361]

--- UI/js/question.js
+++ UI/js/question.js
@@ -1,5 +1,49 @@
 const title = document.querySelector('.title');
 const description = document.querySelector('.description');
 const button = document.querySelector('.submit-btn');
+const error = document.querySelector('.error');
+const details = document.querySelector('.data');
+const token = localStorage.getItem('token');
 
-const va
+const validateInput = () => {
+  if ((title.value === '') || (description.value === '')) {
+    error.style.display = 'flex';
+    details.innerHTML = 'title or description is empty';
+    setTimeout(() => {
+      error.style.display = 'none';
+      details.innerHTML = '';
+    }, 1500);
+  } else {
+    const url = `https://stack-o-lite.herokuapp.com/api/v1/questions?token=${token}`;
+    const data = { 
+      questionTitle: title.value,
+      questionDescription: description.value
+    };
+    fetchData(url, data);
+  }
+}
+
+
+const fetchData = (url, data) => {
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+  }).then(response => {
+    return response.json();
+  }).then(result => {
+    console.log(result);
+    error.style.borderColor = 'green';
+    details.innerHTML = 'question created successfully';
+    setTimeout(() => {
+      error.style.display = 'none';
+      details.innerHTML = '';
+    }, 1500);
+  })
+}
+
+button.addEventListener('click', () => {
+  validateInput();
+});
